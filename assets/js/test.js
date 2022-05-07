97e1a6cd4f6d8b661049374da78e536a613abd87
Benson Jengela <bjengela@iu.edu>
2022-05-07 15:46:25 -0400

    testing4.2

--- assets/js/test.js
+++ assets/js/test.js
@@ -22,13 +22,13 @@ const initData = {
   };
   
   userInput = document.getElementById('search-focus')
-  console.log("userInput: ",userInput);
+  console.log("userInput target: ",userInput.target);
 
-//   if(userInput.target.value.length > 0)
-//   {
-//     instance = new mdb.Datatable(document.getElementById('datatable'), initData)
-//   }
-//   else
+  if(userInput.target.value.length > 0)
+  {
+    instance = new mdb.Datatable(document.getElementById('datatable'), initData)
+  }
+  else
     instance = new mdb.Datatable(document.getElementById('datatable'), data2)
 
   
