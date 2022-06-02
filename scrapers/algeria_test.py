f3229fa551c586da2ea1d94ce03285d53d6882b2
David Pettifor <dpettifo@nd.edu>
2022-06-02 14:21:08 -0400

    started working with json

--- scrapers/algeria_test.py
+++ scrapers/algeria_test.py
@@ -1,5 +1,5 @@
 import requests
-
+import json
 
 post_data = '{"startRecordNum":"0","keyword":"Algeria","oppNum":"","cfda":"","oppStatuses":"forecasted|posted"}'
 
@@ -8,7 +8,30 @@ headers = {
     'Content-Type': 'application/json; charset=utf-8',
 }
 
+# master list
+master_hits = []
+
+# START POPULTAING LIST
 response = requests.post(post_url, headers=headers, data=post_data)
 
-print(response.text)
+json_response = json.loads(response.text)
+
+total_hits = json_response['hitCount']
+
+first_page = json_response['oppHits']
+
+print(total_hits)
+print(len(first_page))
+
+total_hits_ingested = len(first_page)
+current_page = 1
+
+while total_hits_ingested < total_hits:
+    current_page += 1
+    # make new call with updated post_data to change `startRecordNum` = next round (26)
+
+# END POPULATING LIST
+
+# push master_list up to airtable
+#   https://airtable.com/api
 
