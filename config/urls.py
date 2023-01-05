b0a4d8f93b9c1f8723f8561765a6ac6f09942669
ebrinckm <ebrinckm@gmail.com>
2023-01-05 14:43:37 -0500

    fundings page is initialized.

--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,8 @@ urlpatterns = [
     path("users/", include("algeria.users.urls", namespace="users")),
     path("accounts/", include("allauth.urls")),
     # Your stuff: custom urls includes go here
+    path("resourcesPage/", TemplateView.as_view(template_name="pages/resourcePage.html"), name="resourcePage"),
+    path("fundingOpportunities", TemplateView.as_view(template_name="pages/fundingOpportunities.html"), name="fundingOpportunities")
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # API URLS
