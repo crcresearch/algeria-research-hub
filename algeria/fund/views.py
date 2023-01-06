01592d8baddf9e760c66ed8e7519d36b3563aea4
ebrinckm <ebrinckm@gmail.com>
2023-01-06 14:56:34 -0500

    Fixed javascript not found error.

--- algeria/fund/views.py
+++ algeria/fund/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Fund
 
 # Create your views here.
+def funding(request):
+    funding = Fund.objects.order_by("posted_date")
+    print("\n[DEBUG]\n",funding)
+    return render(request, "pages/fundingOpportunities.html", {"funding": funding})
