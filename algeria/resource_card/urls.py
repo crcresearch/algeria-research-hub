b5f9210895e63cdc4551a800eae081dd4085c965
ebrinckm <ebrinckm@gmail.com>
2023-01-18 13:36:40 -0500

    Implemented details page for the resource cards.

--- algeria/resource_card/urls.py
+++ algeria/resource_card/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import resources
+from .views import resources, resource_detail
 
 urlpatterns = [
     path("", resources, name="resourcePage"),
+    path("<int:pk>/", resource_detail, name="resource_detail"),
 ]
--- algeria/resource_card/views.py
+++ algeria/resource_card/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Resource
 # Create your views here.
 def resources(request):
     resources = Resource.objects.order_by("title")
     return render(request, "pages/resourcePage.html", {"resources": resources})
+
+def resource_detail(request, pk):
+    resource = get_object_or_404(Resource, pk=pk)
+    return render(request, "resource/resourceDetail.html", {"resource": resource})
