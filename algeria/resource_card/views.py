86beeb51a387ac22b53e87aaec3f485363502cbe
ebrinckm <ebrinckm@gmail.com>
2023-01-18 15:03:30 -0500

    Roughhsearch bar implemented for resource page.

--- algeria/resource_card/views.py
+++ algeria/resource_card/views.py
@@ -2,7 +2,14 @@ from django.shortcuts import render, get_object_or_404
 from .models import Resource
 # Create your views here.
 def resources(request):
-    resources = Resource.objects.order_by("title")
+    search_resource = ""
+    
+    if "search" in request.GET:
+        search_resource = request.GET["search"]
+        resources = Resource.objects.all().filter(title__icontains=search_resource)
+    else:
+        resources = Resource.objects.order_by("title")
+
     return render(request, "pages/resourcePage.html", {"resources": resources})
 
 def resource_detail(request, pk):
