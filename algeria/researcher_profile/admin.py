9c54a75fd5c127c4febc0e8bde5de627b46a8ff6
claudia <claudiakuczun@gmail.com>
2023-02-06 10:07:20 -0500

    register model in admin & edit model names

--- algeria/researcher_profile/admin.py
+++ algeria/researcher_profile/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Institution)
+admin.site.register(Department)
+admin.site.register(Position)
+admin.site.register(Research_Area)
+admin.site.register(Collaboration_Area)
+admin.site.register(Link)
+admin.site.register(Profile)
--- algeria/researcher_profile/models.py
+++ algeria/researcher_profile/models.py
@@ -26,13 +26,13 @@ class Research_Area(models.Model): ####implement fuzzy matching / standardizatio
         return self.name
 
 
-class Collaboration_Areas(models.Model): ####implement fuzzy matching / standardization of names
+class Collaboration_Area(models.Model): ####implement fuzzy matching / standardization of names
     name = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
 
-class Links(models.Model):
+class Link(models.Model):
     link = models.URLField(max_length = 255, null=True, blank=True)
     link_type = models.CharField(max_length=255)
     link_type = models.CharField(max_length=255)
@@ -43,7 +43,7 @@ class Links(models.Model):
 
 
 
-class Resource(models.Model):
+class Profile(models.Model):
     name = models.CharField(max_length=255)
     image = models.URLField(max_length=255, null=True, blank=True)
     institution = models.ManyToManyField(Institution)
@@ -51,7 +51,7 @@ class Resource(models.Model):
     position = models.ManyToManyField(Position)
     email = models.EmailField
     resume_cv_link = models.URLField(max_length = 255, null=True, blank=True)
-    professional_links = models.ManyToManyField(Links)
+    professional_links = models.ManyToManyField(Link)
     whatsapp = models.CharField(max_length=4096)
     research_areas = models.ManyToManyField(Research_Area, related_name='primary')
     research_overview = models.TextField()
